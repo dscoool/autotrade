fef09ad8d4ab36f30750f47da30497758b22135c
강의게시판 <85654856+dscoool@users.noreply.github.com>
2023-01-17 09:09:46 +0900

    Create autostock.php

---
+++ autostock.php
@@ -0,0 +1,18 @@
+<?xml version='1.0'?>
+<stocks>
+  <stock>
+    <s_run>true</s_run>
+    <s_state>sale</s_state>
+    <s_code>055550</s_code>
+    <s_price>43400</s_price>
+    <s_count>1</s_count>
+  </stock>
+
+  <stock>
+    <s_run>true</s_run>
+    <s_state>buy</s_state>
+    <s_code>055550</s_code>
+    <s_price>42400</s_price>
+    <s_count>1</s_count>
+  </stock>
+</stocks>
